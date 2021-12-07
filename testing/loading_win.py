f6546ed4c50b02083abfc34cc97ef5f1baf1a616
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2021-12-07 09:58:10 -0800

    fixed login bug

---
+++ testing/loading_win.py
@@ -0,0 +1,8 @@
+import dearpygui.dearpygui as dpg
+dpg.create_context()
+
+dpg.create_viewport(title='Custom Title', width=800, height=600)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
